1bed509f65e577fe79b53768620c7b60d0df5e1f
Eklas Mohin <mohin73k@gmail.com>
2025-10-30 14:02:01 +0600

    Uva 100 - The 3n + 1 problem

---
+++ 100.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+unordered_map<int, int> memo;
+
+int solve(int i, int j) {
+    if (i > j) swap(i, j);
+    int result = 0;
+    while (j >= i) {
+        if ((memo.find(i) != memo.end())) {
+            result = max(result, memo[i]);
+            i += 1;
+            continue;
+        }
+        int t = i, c = 1;
+        while (t != 1) {
+            t = t % 2 == 0 ? t / 2 : 3 * t + 1;
+            c += 1;
+        }
+        result = max(result, c);
+        memo[i] = c;
+        i += 1;
+    }
+    return result;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int i, j;
+    while (cin >> i >> j) {
+        int mcl = solve(i, j);
+        cout << i << ' ' << j << ' ' << mcl << '\n';
+    }
+    return 0;
+}
